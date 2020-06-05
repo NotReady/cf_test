25cc9384cfc55d8f926653efe67d6737319bdc16
msohashi <ohashi@musicsecurities.com>
2020-06-05 16:35:08 +0900

    ログインページへのクリック遷移 ログインIDリマインダーへのクリック遷移

--- test/n-ippo.spec.js
+++ test/n-ippo.spec.js
@@ -27,16 +27,63 @@ describe("デモ", () => {
   /**
    * @note トップページを表示し、レスポンスをタイトルの一致で検証します
    */
-  it("サイトルートの疎通試験", async () => {
+  it("サイトルートの疎通テスト", async () => {
     
-    generalUtil.InfoLog("サイトルート");
+    generalUtil.InfoLog("サイトルートの疎通テスト開始");
     await driver.get("https://n-ippo.jp/");
 
     // トップページのロード待ち
     //await driver.wait(until.titleContains('セキュリテ - インパクト投資プラットフォーム'), 10000);
     
     await driver.getTitle().then(function (title) {
-      expect(title).toBe("セキュリテ - インパクト投資プラットフォーム");
+      expect(title).toBe("にいがた、いっぽ　新しいことを始めたいあなたと、夢のあるプロジェクトを応援したいあなたをつなぎます。");
     });
+
+    generalUtil.InfoLog("サイトルートの疎通テスト完了");
   });
+
+  /**
+   * @note ログインページへのクリック遷移を検証します
+   */
+  it("ログインページの遷移テスト", async () => {
+
+    generalUtil.InfoLog("ログインページの疎通テスト開始");
+
+    // ログインのリンク要素
+    var aButton = await driver.findElement(By.xpath("/html/body/header/div/div/div/span/a[2]"));
+    var href = await aButton.getAttribute("href");
+
+    // クリック遷移
+    await aButton.click();
+    var checkUrl = await driver.getCurrentUrl();
+
+    // URL一致で疎通確認
+    expect(checkUrl).toBe(href);
+
+    generalUtil.InfoLog("ログインページの疎通テスト完了");
+  });
+
+  /**
+   * @note ログインIDをお忘れの方へのクリック遷移を検証します
+   */
+  it("ログインIDをお忘れの方遷移テスト", async () => {
+
+    generalUtil.InfoLog("ログインIDをお忘れの方遷移テスト開始");
+    
+    // ログインIDをお忘れの方リンク要素
+    aButton = await driver.findElement(By.xpath('//*[@id="registration-page"]/div/div[2]/fieldset/form/div[1]/div/p/a'));
+    var href = await aButton.getAttribute("href");
+
+    // クリック遷移
+    await aButton.click();
+    var checkUrl = await driver.getCurrentUrl();
+
+    // URL一致で疎通確認
+    expect(checkUrl).toBe(href);
+
+    generalUtil.InfoLog("ログインIDをお忘れの方遷移テスト完了");
+  });
+
+
+  
 });
